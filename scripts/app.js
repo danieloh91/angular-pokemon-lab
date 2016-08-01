4ee13b30e1286559557393959958eefe3527fcb1
danieloh91 <daniel.oh.91@gmail.com>
2016-07-31 20:04:36 -0700

    created GET request to display pokemon

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,24 @@
 console.log('app.js loaded!');
+
+angular
+      .module("pokeApp", [])
+      .controller("pokemonController", pokemonController);
+
+pokemonController.$inject=['$http'];
+
+function pokemonController ($http) {
+  var vm = this;
+
+  vm.newPoke = {};
+
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/pokemon'
+  }).then(function successCallback(response) {
+    vm.pokemon = response.data.pokemon;
+    console.log(vm.pokemon);
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+  });
+
+}
