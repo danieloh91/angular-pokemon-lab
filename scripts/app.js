c1645909ee64a2f472bf68a71f3dfed7847e29dd
danieloh91 <daniel.oh.91@gmail.com>
2016-07-31 20:35:57 -0700

    created POST method to add new pokemon to database

--- scripts/app.js
+++ scripts/app.js
@@ -16,9 +16,25 @@ function pokemonController ($http) {
     url: 'https://super-crud.herokuapp.com/pokemon'
   }).then(function successCallback(response) {
     vm.pokemon = response.data.pokemon;
-    console.log(vm.pokemon);
   }, function errorCallback(response) {
     console.log('There was an error getting the data', response);
   });
+
+  vm.createPoke = function () {
+    $http({
+      method: 'POST',
+      url: 'https://super-crud.herokuapp.com/pokemon',
+      data: vm.newPoke
+    }).then(function successCallback(response) {
+      vm.pokemon.push(response.data);
+      console.log(response.data);
+    }, function errorCallback(response) {
+      console.log('There was an error posting the data', response);
+    });
+  };
+
+  vm.deletePoke = function () {
+    
+  };
 
 }
